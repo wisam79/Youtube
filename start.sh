f2ffb77799c4f161ad8b140322d02397c1b02b6b
wisam79 <73247302+wisam79@users.noreply.github.com>
2021-10-05 17:18:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/wisam79/Youtube
+git clone https://github.com/wisam79/Youtube.git /AnyDLBot
 cd /Youtube
 pip3 install -r requirements.txt
 echo "Starting Bot...."
